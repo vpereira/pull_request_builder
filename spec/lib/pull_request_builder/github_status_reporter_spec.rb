b4660f06ef36926363f5d879eb5ae2e5895293f9
Victor Pereira <vpereira@suse.de>
2019-02-25 15:06:43 +0100

    Refactor github_status_reporter spec

--- spec/lib/pull_request_builder/github_status_reporter_spec.rb
+++ spec/lib/pull_request_builder/github_status_reporter_spec.rb
@@ -5,16 +5,23 @@ require 'spec_helper'
 include PullRequestBuilder
 
 RSpec.describe GitHubStatusReporter, :vcr do
+
+  let(:client) do
+    Octokit::Client.new({access_token: '6fccee211b93aae7aa24b0263d05d59cdx4a9ec1', 
+                         logging: false})
+  end
+
+  let(:github_status_reporter) do
+      described_class.new(repository: 'vpereira/hello_world', package: 'hello_world', client: client)
+  end
+
+
   describe '.initialize' do
     it { expect(described_class.new(repository: 'foo', package: 'bar')).not_to be_nil }
     it { expect { described_class.new(bar: 'foo') }.to raise_error(ActiveModel::UnknownAttributeError) }
   end
 
   describe '.state' do
-    let(:github_status_reporter) do
-      described_class.new(repository: 'foo', package: 'bar')
-    end
-
     let(:summary_success) do
       { failure: 0, success: 1, pending: 0 }
     end
@@ -25,5 +32,4 @@ RSpec.describe GitHubStatusReporter, :vcr do
 
     it { expect(github_status_reporter.send(:state)).to eq(:success) }
   end
-
 end
