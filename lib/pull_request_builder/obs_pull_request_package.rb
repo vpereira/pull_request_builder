4ff10178f6eefa497970669fde60ace532b39d95
Victor Pereira <vpereira@suse.de>
2019-03-04 13:21:20 +0100

    Remove the unused method capture2e_with_logs

--- lib/pull_request_builder/obs_pull_request_package.rb
+++ lib/pull_request_builder/obs_pull_request_package.rb
@@ -98,18 +98,6 @@ module PullRequestBuilder
       end
     end
 
-    def capture2e_with_logs(cmd)
-      logger.info("Execute command '#{cmd}'.")
-      stdout_and_stderr_str, status = Open3.capture2e(cmd)
-      stdout_and_stderr_str.chomp!
-      if status.success?
-        logger.info(stdout_and_stderr_str)
-      else
-        logger.error(stdout_and_stderr_str)
-      end
-      stdout_and_stderr_str
-    end
-
     def create_project
       send_meta_file("#{pull_request_number}-meta", operation: :prj)
     end
